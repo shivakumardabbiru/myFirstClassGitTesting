1c1e9149578e6e436c595a220cff6bb46bc6e672
shivakumardabbiru <65832760+shivakumardabbiru@users.noreply.github.com>
2020-06-17 23:17:55 +0100

    Update myFirstClass.java

--- myFirstClass.java
+++ myFirstClass.java
@@ -1,5 +1,5 @@
 public class myFirstClass{
   public static void main (String[] args) {
-   System.out.println("****updating for Webhooks******");
+   System.out.println("****updating for Webhook******");
  }
 }
